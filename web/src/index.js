20c3e547e996cec79520ae509a0bc74e67264943
Karl Köörna <karlkoorna@gmail.com>
2018-03-31 14:06:05 +0300

    Replace Split with Loadable

--- web/src/index.js
+++ web/src/index.js
@@ -2,16 +2,38 @@ import React, { Fragment } from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import Loadable from 'react-loadable';
 
-import Split from './components/Split';
-import Map from './components/Map';
-import NavBar from './components/NavBar/NavBar';
+import Map from 'containers/Map';
+import NavBar from 'components/NavBar/NavBar';
+import Loading from 'components/Loading/Loading';
+
 import './index.css';
 
-setup();
-async function setup() {
+const Search = Loadable({
+	loader: () => import('containers/Search/Search'),
+	loading: Loading
+});
+
+const Favorites = Loadable({
+	loader: () => import('containers/Favorites/Favorites'),
+	loading: Loading
+});
+
+const Settings = Loadable({
+	loader: () => import('containers/Settings/Settings'),
+	loading: Loading
+});
+
+const Stop = Loadable({
+	loader: () => import('containers/Stop/Stop'),
+	loading: Loading
+});
+
+init();
+async function init() {
 	
-	window.stops = await (await fetch(`https://bussiaeg.ee/getstops?lat_min=60.0&lng_min=60.0&lat_max=20.0&lng_max=20.0`)).json();
+	window.stops = await (await fetch('https://bussiaeg.ee/getstops?lat_min=60.0&lng_min=60.0&lat_max=20.0&lng_max=20.0')).json();
 	
 	render((
 		<BrowserRouter>
@@ -19,13 +41,14 @@ async function setup() {
 				<Route render={({ location }) => (
 					<TransitionGroup id="pages" className={location.pathname === '/' ? 'is-empty' : ''}>
 						<CSSTransition key={location.pathname} classNames="page" timeout={250}>
-								<div id="page">
-									<Switch location={location}>
-										<Route path="/stop" component={() => <Split path="./Stop/Stop" />} />
-										<Route path="/directions" component={() => () => <Split path="./Search/Search" />} />
-										<Route path="/settings" component={() => () => <Split path="./Settings/Settings" />} />
-									</Switch>
-								</div>
+							<div id="page">
+								<Switch location={location}>
+									<Route path="/search" component={Search} />
+									<Route path="/favorites" component={Favorites} />
+									<Route path="/settings" component={Settings} />
+									<Route path="/stop" component={Stop} />
+								</Switch>
+							</div>
 						</CSSTransition>
 					</TransitionGroup>
 				)} />
