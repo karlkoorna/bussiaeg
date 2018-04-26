59929c93cbe0e66e3a8bbd136ce13e82001e2c91
Karl Köörna <karlkoorna@gmail.com>
2018-04-26 17:56:21 +0300

    Semanticize NavBar (web)

--- web/src/components/NavBar/NavBar.jsx
+++ web/src/components/NavBar/NavBar.jsx
@@ -6,12 +6,12 @@ import './NavBar.css';
 
 export default function NavBar() {
 	return (
-		<div id="navbar">
+		<nav id="navbar">
 			<NavBarItem to="/search" colors={[ '#ffa94d', '#ff8400' ]}>Search</NavBarItem>
 			<NavBarItem to="/favorites" colors={[ '#f5557e', '#f22559' ]}>Favorites</NavBarItem>
 			<NavBarItem to="/" colors={[ '#00e6ad', '#00cc9a' ]}>Map</NavBarItem>
 			<NavBarItem to="/settings" colors={[ '#00bfff', '#00ace6' ]}>Settings</NavBarItem>
-		</div>
+		</nav>
 	);
 }
 
@@ -45,7 +45,7 @@ class NavBarItem extends PureComponent {
 						search: (
 							<Fragment>
 								<path stroke={secondaryColor} strokeWidth="125" d="M650.7 650.7l321 321" />
-								<circle fill="none" stroke={primaryColor} strokeWidth="100" cx="399.3" cy="399.3" r="347" />
+								<circle fill="transparent" stroke={primaryColor} strokeWidth="100" cx="399.3" cy="399.3" r="347" />
 							</Fragment>
 						),
 						favorites: (
@@ -62,7 +62,7 @@ class NavBarItem extends PureComponent {
 						settings: (
 							<Fragment>
 								<path fill={primaryColor} d="M512.1 683c-94.4 0-171-76.6-171-171s76.6-171 171-171 171 76.6 171 171-76.6 171-171 171m363-123.6c2-15.6 3.4-31.3 3.4-47.4s-1.5-32.2-3.4-48.9l103.1-79.6c9.3-7.3 11.7-20.5 5.9-31.3l-97.7-169.1c-5.9-10.7-19.1-15.1-29.8-10.7l-121.7 48.9c-25.4-19.1-51.8-35.7-82.6-47.9l-18-129.5c-2-11.7-12.2-20.5-24.4-20.5H414.4c-12.2 0-22.5 8.8-24.4 20.5l-18.1 129.5c-30.8 12.2-57.2 28.8-82.6 47.9l-121.7-48.9c-10.7-4.4-23.9 0-29.8 10.7L40.1 352.2c-6.4 10.7-3.4 23.9 5.9 31.3l103.1 79.6c-2 16.6-3.4 32.7-3.4 48.9s1.5 31.8 3.4 47.4L46 640.5c-9.3 7.3-12.2 20.5-5.9 31.3l97.7 169.1c5.9 10.7 19.1 14.7 29.8 10.7l121.7-49.3c25.4 19.5 51.8 36.2 82.6 48.4L390 980.1c2 11.7 12.2 20.5 24.4 20.5h195.4c12.2 0 22.5-8.8 24.4-20.5l18.1-129.5c30.8-12.7 57.2-28.8 82.6-48.4l121.7 49.3c10.7 3.9 23.9 0 29.8-10.7l97.7-169.1c5.9-10.7 3.4-23.9-5.9-31.3l-103.1-81z" />
-								<circle fill="none" stroke={secondaryColor} strokeWidth="100" cx="512" cy="512" r="205.6" />
+								<circle fill="transparent" stroke={secondaryColor} strokeWidth="100" cx="512" cy="512" r="205.6" />
 							</Fragment>
 						)
 					}[children.toLowerCase()]}
