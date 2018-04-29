3be5b8aaf4ec12fb42a6bad9c59af0421624e4b6
Karl Köörna <karlkoorna@gmail.com>
2018-04-29 13:18:11 +0300

    Cleanup (web)

--- web/src/components/Loading/Loading.jsx
+++ web/src/components/Loading/Loading.jsx
@@ -7,9 +7,9 @@ import './Loading.css';
 export default function Loading() {
 	return (
 		<div id="loading">
-			<VehicleIcon id="loading-bus" className="loading-vehicle" type="bus" />
-			<VehicleIcon id="loading-trol" className="loading-vehicle" type="trol" />
-			<VehicleIcon id="loading-tram" className="loading-vehicle" type="tram" />
+			{VehicleIcon({ id: 'loading-bus', className: 'loading-vehicle', type: 'bus' })}
+			{VehicleIcon({ id: 'loading-trol', className: 'loading-vehicle', type: 'trol' })}
+			{VehicleIcon({ id: 'loading-tram', className: 'loading-vehicle', type: 'tram' })}
 		</div>
 	);
 }
