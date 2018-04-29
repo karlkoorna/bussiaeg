6706de9e58bdd7f6f39d3411217161a5a8a269c9
Karl Köörna <karlkoorna@gmail.com>
2018-04-29 12:51:18 +0300

    Add Modal state animations (web)

--- web/src/components/Modal/Modal.jsx
+++ web/src/components/Modal/Modal.jsx
@@ -1,23 +1,50 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import { CSSTransition } from 'react-transition-group';
 
 import './Modal.css';
 
-export default function Model({ isVisible, title, text, onCancel, onConfirm, children }) {
+export default class Model extends PureComponent {
 	
-	return (
-		<CSSTransition in={isVisible} classNames={{ enter: 'is-entering', exit: 'is-exiting' }} timeout={250} unmountOnExit>
-			<div id="modal" onClick={onCancel}>
-				<div id="modal-panel" onClick={(e) => { e.stopPropagation() }}>
-					<div id="modal-panel-title">{title}</div>
-					<div id="modal-panel-text">{text}</div>
-					<div id="modal-panel-buttons">
-						<div id="modal-panel-buttons-cancel" onClick={onCancel}>Tühista</div>
-						<div id="modal-panel-buttons-confirm" onClick={onConfirm}>Kinnita</div>
+	state = {
+		action: ''
+	}
+	
+	onClick = this.onClick.bind(this)
+	onExited = this.onExited.bind(this)
+	
+	onClick(e) {
+		
+		const action = e.target.id.substr(27);
+		
+		this.props[`on${action[0].toUpperCase() + action.substr(1)}`]();
+		this.setState({ action });
+		
+	}
+	
+	onExited() {
+		this.setState({ action: '' });
+	}
+	
+	render() {
+		
+		const { isVisible, title, text, onCancel } = this.props;
+		
+		return (
+			<CSSTransition in={isVisible} classNames={{ enter: 'is-entering', exit: 'is-exiting' }} timeout={250} onExited={this.onExited} unmountOnExit>
+				<div id="modal" onClick={onCancel}>
+					<div id="modal-panel" className={this.state.action} onClick={(e) => { e.stopPropagation() }}>
+						<div id="modal-panel-"></div>
+						<div id="modal-panel-title">{title}</div>
+						<div id="modal-panel-text">{text}</div>
+						<div id="modal-panel-buttons">
+							<div id="modal-panel-buttons-button-cancel" onClick={this.onClick}>Tühista</div>
+							<div id="modal-panel-buttons-button-confirm" onClick={this.onClick}>Kinnita</div>
+						</div>
 					</div>
 				</div>
-			</div>
-		</CSSTransition>
-	);
+			</CSSTransition>
+		);
+		
+	}
 	
 }
