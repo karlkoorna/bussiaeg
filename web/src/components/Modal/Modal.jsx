cbd15c174d3c172fe99fff591e80c1684aef560a
Karl Köörna <karlkoorna@gmail.com>
2018-06-03 17:20:56 +0300

    Refactor Modal (web)

--- web/src/components/Modal/Modal.jsx
+++ web/src/components/Modal/Modal.jsx
@@ -5,56 +5,33 @@ import './Modal.css';
 
 export default class Model extends PureComponent {
 	
-	state = {
-		action: ''
+	// Add animation class to button and call function.
+	onClick =(e, cb)=> {
+		e.nativeEvent.target.classList.add('is-active');
+		cb();
 	}
 	
-	onClick = this.onClick.bind(this)
-	onExited = this.onExited.bind(this)
-	onKeyDown = this.onKeyDown.bind(this)
-	
-	onClick(e) {
-		
-		const { onCancel, onConfirm } = this.props;
-		const action = e.target.id.substr(27);
-		
-		if (action === 'cancel') onCancel();
-		else if (action === 'confirm') onConfirm();
-		
-		this.setState({ action });
-		
-	}
-	
-	onKeyDown(e) {
+	// Cancel modal on escape key.
+	onKeyDown =(e)=> {
 		if (e.which === 27) this.props.onCancel();
 	}
 	
-	onExited() {
-		this.setState({ action: '' });
-	}
-	
+	// Toggle keydown listener based on visibility.
 	componentDidUpdate() {
-		
-		if (this.state.isVisible) return void document.addEventListener('keydown', this.onKeyDown);
-		
-		document.removeEventListener('keydown', this.onKeyDown);
-		
-	}
-	
-	componentWillUnmount() {
+		if (this.props.isVisible) return document.addEventListener('keydown', this.onKeyDown);
 		document.removeEventListener('keydown', this.onKeyDown);
 	}
 	
 	render() {
 		return (
 			<CSSTransition in={this.props.isVisible} classNames={{ enter: 'is-entering', exit: 'is-exiting' }} timeout={250} onExited={this.onExited} unmountOnExit>
-				<div id="modal" onClick={this.onCancel}>
-					<div id="modal-panel" className={this.state.action} onClick={(e) => { e.stopPropagation() }}>
+				<div id="modal" onClick={this.props.onCancel}>
+					<div id="modal-panel" onClick={(e) => { e.stopPropagation() }}>
 						<div id="modal-panel-title">{this.props.title}</div>
 						<div id="modal-panel-text">{this.props.text}</div>
 						<div id="modal-panel-buttons">
-							<div id="modal-panel-buttons-button-cancel" className={this.state.action === 'cancel' ? 'is-active' : null} onClick={this.onClick}>Tühista</div>
-							<div id="modal-panel-buttons-button-confirm" className={this.state.action === 'confirm' ? 'is-active' : null} onClick={this.onClick}>Kinnita</div>
+							<div id="modal-panel-buttons-button-cancel" onClick={(e) => this.onClick(e, this.props.onCancel)}>Tühista</div>
+							<div id="modal-panel-buttons-button-confirm" onClick={(e) => this.onClick(e, this.props.onConfirm)}>Kinnita</div>
 						</div>
 					</div>
 				</div>
