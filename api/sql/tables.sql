6e7da50071f4137f97e7e3875244711788b6bdc7
Karl Köörna <karlkoorna@gmail.com>
2018-06-24 20:38:34 +0300

    Specify not nullable for trips columns (api)

--- api/sql/tables.sql
+++ api/sql/tables.sql
@@ -28,7 +28,7 @@ CREATE TABLE trips (
 	id INT(16) NOT NULL,
 	route_id CHAR(32) NOT NULL,
 	service_id INT(16) NOT NULL,
-	wheelchair BOOL,
+	wheelchair BOOL NOT NULL,
 	PRIMARY KEY (id),
 	UNIQUE (id)
 );
