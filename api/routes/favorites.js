59202aa0df3efea813a51b9dc726ac8bc66750a3
Karl Köörna <karlkoorna@gmail.com>
2018-06-28 22:56:01 +0300

    Add validation rules to favorites route (api)

--- api/routes/favorites.js
+++ api/routes/favorites.js
@@ -16,9 +16,13 @@ async function getFavorites(req, res) {
 async function postFavorites(req, res) {
 	
 	try {
+		
+		// Generate a random 4 character upper-case code.
 		const id = Math.random().toString(36).substr(2, 4).toUpperCase();
+		
 		await db.query('INSERT INTO favorites (id, data) VALUES (?, ?)', [ id, JSON.stringify(req.body) ]);
 		res.send(id);
+		
 	} catch (ex) {
 		res.code(500).send(err);
 	}
@@ -34,13 +38,14 @@ module.exports = (fastify, opts, next) => {
 			body: {
 				type: 'array',
 				minItems: 1,
+				maxItems: 32,
 				items: {
 					type: 'object',
 					required: [ 'id', 'name', 'direction' ],
 					properties: {
-						id: { type: 'string', minLength: 3 },
-						name: { type: 'string', minLength: 1 },
-						direction: { type: 'string', minLength: 1 }
+						id: { type: 'string', minLength: 3, maxLength: 32 },
+						name: { type: 'string', minLength: 1, maxLength: 32 },
+						direction: { type: 'string', minLength: 1, maxLength: 32 }
 					}
 				}
 			}
