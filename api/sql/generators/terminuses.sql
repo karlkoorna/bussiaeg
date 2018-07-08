18637a0de4063765285d21aa1e78046b3ff61252
Karl Köörna <karlkoorna@gmail.com>
2018-07-08 12:48:31 +0300

    Fix terminus generation (api)

--- api/sql/generators/terminuses.sql
+++ api/sql/generators/terminuses.sql
@@ -1,6 +1,7 @@
-SELECT * FROM trips AS trip
+UPDATE trips AS trip
 JOIN stop_times ON trip_id = trip.id
 JOIN stops AS stop ON stop.id = stop_id
+SET terminus = name
 WHERE (trip_id, sequence) IN (
 	SELECT trip_id, MAX(sequence) FROM stop_times WHERE trip_id = trip.id
 );
