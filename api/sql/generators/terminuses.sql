72e5833e03592844231e38fbae162bbe422de6a1
Karl Köörna <karlkoorna@gmail.com>
2018-07-10 12:54:49 +0300

    Normalize terminus names (api)

--- api/sql/generators/terminuses.sql
+++ api/sql/generators/terminuses.sql
@@ -1,7 +1,7 @@
 UPDATE trips AS trip
 JOIN stop_times ON trip_id = trip.id
 JOIN stops AS stop ON stop.id = stop_id
-SET terminus = name
+SET terminus = REPLACE(name, ' parkla', '')
 WHERE (trip_id, sequence) IN (
 	SELECT trip_id, MAX(sequence) FROM stop_times WHERE trip_id = trip.id
 );
