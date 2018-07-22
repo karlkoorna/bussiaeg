ffc10e50e1a153a01cb9e2afde83e929d7bc666b
Karl Köörna <karlkoorna@gmail.com>
2018-07-22 14:57:24 +0300

    Add search route (api)

---
+++ api/routes/search.js
@@ -0,0 +1,42 @@
+const db = require('../db.js');
+
+// Get favorites by id.
+async function getSearch(req, res) {
+	
+	res.send(await db.query(`
+		SELECT
+			id,
+    		name,
+    		type,
+    		region,
+			ROUND(ST_Distance_Sphere(PointFromText(CONCAT('POINT(', lat, ' ', lng, ')')), PointFromText('POINT(? ?)'))) AS distance
+		FROM stops
+		WHERE
+			type IS NOT NULL
+			AND name LIKE ?
+		ORDER BY distance
+		LIMIT ?
+	`, [ req.query['lat'], req.query['lng'], `%${req.query['query']}%`, req.query['limit'] ]));
+	
+}
+
+module.exports = (fastify, opts, next) => {
+	
+	fastify.get('/search', {
+		schema: {
+			querystring: {
+				type: 'object',
+				required: [ 'query', 'lat', 'lng', 'limit' ],
+				properties: {
+					query: { type: 'string', minLength: 1 },
+					lat: { type: 'number' },
+					lng: { type: 'number' },
+					limit: { type: 'number' },
+				}
+			}
+		}
+	}, getSearch);
+	
+	next();
+	
+};
