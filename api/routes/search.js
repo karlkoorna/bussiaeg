e787f17fc527f8ee18c26ef24ebc1692c0c596ae
Karl Köörna <karlkoorna@gmail.com>
2018-08-19 23:01:26 +0300

    Add search route (api)

--- api/routes/search.js
+++ api/routes/search.js
@@ -3,20 +3,43 @@ const db = require('../db.js');
 // Get favorites by id.
 async function getSearch(req, res) {
 	
-	res.send(await db.query(`
-		SELECT
-			id,
-			name,
-			type,
-			region,
-			ROUND(ST_Distance_Sphere(PointFromText(CONCAT('POINT(', lat, ' ', lng, ')')), PointFromText('POINT(? ?)'))) AS distance
-		FROM stops
-		WHERE
-			type IS NOT NULL
-			AND name LIKE ?
-		ORDER BY LOCATE(?, name), distance
-		LIMIT ?
-	`, [ req.query['lat'], req.query['lng'], `%${req.query['query']}%`, req.query['query'], req.query['limit'] ]));
+	const { query, type, lat, lng } = req.query;
+	
+	switch (type) {
+		
+		case 'stops': {
+			
+			return void res.send(await db.query(`
+				SELECT
+					id, name, direction, type
+					${lat && lng ? ",ROUND(ST_Distance_Sphere(PointFromText(CONCAT('POINT(', lat, ' ', lng, ')')), PointFromText('POINT(? ?)'))) AS distance" : ''}
+				FROM stops
+				WHERE
+					type IS NOT NULL
+					AND name LIKE ?
+				ORDER BY ${lat && lng ? 'distance' : ''}
+				LIMIT 30
+			`, lat && lng ? [ lat, lng, `%${query}%` ] : [ `%${query}%` ]));
+			
+		}
+		
+		case 'routes': {
+			
+			return void res.send(await db.query(`
+				SELECT
+					id, name, origin, destination, type,
+					ROUND(ST_Distance_Sphere(PointFromText(CONCAT('POINT(', lat, ' ', lng, ')')), PointFromText('POINT(? ?)'))) AS distance
+				FROM routes
+				WHERE
+					type IS NOT NULL
+					AND name LIKE ?
+				ORDER BY distance
+				LIMIT 30
+			`, [ lat, lng, `%${query}%` ]));
+			
+		}
+		
+	}
 	
 }
 
@@ -26,12 +49,12 @@ module.exports = (fastify, opts, next) => {
 		schema: {
 			querystring: {
 				type: 'object',
-				required: [ 'query', 'lat', 'lng', 'limit' ],
+				required: [ 'type' ],
 				properties: {
-					query: { type: 'string', minLength: 1 },
+					query: { type: 'string' },
+					type: { type: 'string', enum: [ 'stops', 'routes' ] },
 					lat: { type: 'number' },
-					lng: { type: 'number' },
-					limit: { type: 'number' },
+					lng: { type: 'number' }
 				}
 			}
 		}
