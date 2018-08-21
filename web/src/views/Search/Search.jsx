e4b1c57d98fb39c1504a4e117319466e8f06a320
Karl Köörna <karlkoorna@gmail.com>
2018-08-21 12:26:55 +0300

    Refactor Search (web)

--- web/src/views/Search/Search.jsx
+++ web/src/views/Search/Search.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import Swipeable  from 'react-swipeable';
 import Ink from 'react-ink';
 
 import VehicleIcon, { colors } from 'components/VehicleIcon.jsx';
@@ -14,10 +15,36 @@ export default class Search extends Component {
 	state = {
 		query: '',
 		type: 'stops',
-		results: []
+		results: {}
 	}
 	
 	debounce = null
+	
+	// Fetch results by query, type and coords (if available).
+	fetch = async () => {
+	
+		const { query, type } = this.state;
+		const { lat, lng } = storeCoords.get();
+		
+		try {
+			this.setState({ results: !lat && !lng && !query ? {} : await (await fetch(`${process.env['REACT_APP_API']}/search?${query ? `&query=${query}` : ''}${lat && lng ? `&lat=${lat}&lng=${lng}` : ''}`)).json() });
+		} catch (ex) {
+			this.setState({ results: {} });
+		}
+		
+	}
+	
+	// Update query and fetch new results.
+	updateQuery = (e) => {
+	
+		const query = e.target.value;
+		
+		clearTimeout(this.debounce);
+		this.debounce = setTimeout(() => {
+			this.setState({ query }, this.fetch);
+		}, 300);
+		
+	}
 	
 	// Hide keyboard if enter key pressed (on mobile).
 	hideKeyboard = (e) => {
@@ -30,35 +57,14 @@ export default class Search extends Component {
 		this.setState({ query: '' }, this.fetch);
 	}
 	
-	// Update query and fetch new results.
-	updateQuery = (e) => {
-		
-		const query = e.target.value;
-		
-		clearTimeout(this.debounce);
-		this.debounce = setTimeout(() => {
-			this.setState({ query }, this.fetch);
-		}, 300);
-		
-	}
-	
-	// Update type, clear previous and fetch new results.
+	// Update type.
 	updateType = (type) => {
-		this.setState({ type, results: [] }, this.fetch);
+		this.setState({ type });
 	}
 	
-	// Fetch results by query, type and coords (if available).
-	fetch = async () => {
-		
-		const { query, type } = this.state;
-		const { lat, lng } = storeCoords.get();
-		
-		try {
-			this.setState({ results: !lat && !lng && !query ? [] : await (await fetch(`${process.env['REACT_APP_API']}/search?${query ? `&query=${query}` : ''}&type=${type}${lat && lng ? `&lat=${lat}&lng=${lng}` : ''}`)).json() });
-		} catch (ex) {
-			this.setState({ results: [] });
-		}
-		
+	// Update type.
+	swipeType = (e, deltaX, deltaY, absX, absY, velocity) => {
+		if (velocity > .5 && Math.abs(deltaX) > Math.abs(deltaY) * 1.5) this.updateType(deltaX < 0 ? 'stops' : 'routes');
 	}
 	
 	async componentWillMount() {
@@ -73,36 +79,40 @@ export default class Search extends Component {
 			<section id="search" className={`view${ this.props.isActive ? ' is-visible': ''}`}>
 				<div id="search-top">
 					<input id="search-top-input" placeholder="Search..." autoComplete="off" required ref="$query" onKeyDown={this.hideKeyboard} onInput={this.updateQuery} />
-					<svg xmlns="http://www.w3.org/2000/svg" style={this.state} viewBox="0 0 1024 1024">
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024">
 						<path stroke="#b3b3b3" strokeWidth="125" d="M650.7 650.7l321 321" />
 						<circle fill="transparent" stroke="#bdbdbd" strokeWidth="100" cx="399.3" cy="399.3" r="347" />
 					</svg>
-					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024" onClick={this.clearQuery}>
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1024 1024" onMouseDown={this.clearQuery}>
 						<path stroke="#b3b3b3" strokeWidth="128" d="M92 92l840 840M932 92L92 932" />
 					</svg>
 					<div id="search-top-types">
-						<div className={'search-top-types-item' + (type === 'stops' ? ' is-active' : '')} onClick={() => this.updateType('stops')}>Stops
+						<div className={'search-top-types-item' + (type === 'stops' ? ' is-active' : '')} onMouseDown={() => { this.updateType('stops'); }}>Stops
 							<Ink hasTouch={false} background={false} opacity={.5} style={{ color: '#ffa94d' }} />
 						</div>
-						<div className={'search-top-types-item' + (type === 'routes' ? ' is-active' : '')} onClick={() => this.updateType('routes')}>Routes
+						<div className={'search-top-types-item' + (type === 'routes' ? ' is-active' : '')} onMouseDown={() => { this.updateType('routes'); }}>Routes
 							<Ink hasTouch={false} background={false} opacity={.5} style={{ color: '#ffa94d' }} />
 						</div>
 					</div>
 				</div>
-				<div id="search-results">
-					{results.map((result, i) => (
-						<div className="search-results-result-container" key={i}>
-							<Link className="search-results-result" to={`/stop?id=${result.id}`}>
-								{{ stops: StopIcon, routes: VehicleIcon }[type]({ className: `search-results-result-icon is-${type}`, type: result.type })}
-								<div style={{ color: colors[result.type][0] }}>
-									<div className="search-results-result-name">{result.name}</div>
-									<div className="search-results-result-area">{result.direction || (result.origin && result.destination ? `${result.origin} - ${result.destination}` : '')}</div>
+				<Swipeable id="search-results" flickThreshold={.3} onSwiping={this.swipeType} style={{ transform: type === 'stops' ? 'translateX(0)' : 'translateX(-100%)' }}>
+					{[ 'stops', 'routes' ].map((type, i) => (
+						<div key={i}>
+							{results[type] ? results[type].map((result, j) => (
+								<div className="search-results-result-container" key={j}>
+									<Link className="search-results-result" to={`/stop?id=${result.id}`}>
+										{{ stops: StopIcon, routes: VehicleIcon }[type]({ className: `search-results-result-icon is-${type}`, type: result.type })}
+										<div style={{ color: colors[result.type][0] }}>
+											<div className="search-results-result-name">{result.name}</div>
+											<div className="search-results-result-area">{result.direction || (result.origin && result.destination ? `${result.origin} - ${result.destination}` : '')}</div>
+										</div>
+										<div className="search-results-result-distance">{result.distance ? result.distance >= 100000 ? `${Math.round(result.distance / 10000) * 10}km` : result.distance >= 10000 ? `${(result.distance / 1000).toFixed()}km` : result.distance >= 1000 ? `${(result.distance / 1000).toFixed(1)}km` : `${Math.round(result.distance / 10) * 10}m` : ''}</div>
+									</Link>
 								</div>
-								<div className="search-results-result-distance">{result.distance ? result.distance >= 100000 ? `${Math.round(result.distance / 10000) * 10}km` : result.distance >= 10000 ? `${(result.distance / 1000).toFixed()}km` : result.distance >= 1000 ? `${(result.distance / 1000).toFixed(1)}km` : `${Math.round(result.distance / 10) * 10}m` : ''}</div>
-							</Link>
+							)) : null}
 						</div>
 					))}
-				</div>
+				</Swipeable>
 			</section>
 		);
 		
