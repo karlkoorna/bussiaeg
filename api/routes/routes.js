ccfd1c5f6fb9128db89dd2e328f0180d8d225edb
Karl Köörna <karlkoorna@gmail.com>
2018-09-05 20:43:12 +0300

    Implement cache in routes route (api)

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -7,42 +7,47 @@ const elron = require('../providers/elron.js');
 // Get route by parameters.
 async function getRoutes(req, res) {
 	
-	switch (req.query['provider']) {
+	const { stop, name, type, provider } = req.query;
+	
+	res.send(await cache.use('routes', `${stop}-${name}-${type}`, async () => {
 		
-		case 'mnt':
-		case 'tlt': {
+		switch (provider) {
 			
-			// Get stop sequences for route.
-			const routes = _.groupBy(await db.query(`
-				SELECT stop_id, name, type, origin, destination FROM stop_times
-				JOIN stops AS stop ON stop.id = stop_id
-				JOIN trips AS trip ON trip.id = trip_id
-				WHERE
-					(trip_id, destination) IN (
-						SELECT trip_id, destination FROM trips AS trip
+			case 'mnt':
+			case 'tlt': {
+				
+				// Get stop sequences for route.
+				const routes = _.groupBy(await db.query(`
+					SELECT stop_id, name, type, origin, destination FROM stop_times
+					JOIN stops AS stop ON stop.id = stop_id
+					JOIN trips AS trip ON trip.id = trip_id
+					WHERE (trip_id, destination) IN (
+						SELECT trip_id, trip.destination FROM trips AS trip
 						JOIN routes AS route ON route.id = trip.route_id
 						JOIN stop_times ON trip_id = trip.id
 						WHERE stop_id = ? AND name = ? AND type = ?
 					)
-				GROUP BY route_id, destination, sequence
-			`, [ req.query['stop'], req.query['name'], req.query['type'] ]), (route) => `${route.origin} - ${route.destination}`);
+					GROUP BY route_id, destination, sequence
+				`, [ stop, name, type ]), (route) => `${route.origin} - ${route.destination}`);
+				
+				// Transform stop objects.
+				for (const key in routes) routes[key] = routes[key].map((stop) => ({
+					id: stop.stop_id,
+					name: stop.name,
+					type: stop.type
+				}));
+				
+				return routes;
+				
+			}
 			
-			// Transform stop objects.
-			for (const key in routes) routes[key] = routes[key].map((stop) => ({
-				id: stop.stop_id,
-				name: stop.name,
-				type: stop.type
-			}));
-			
-			return void res.send(routes);
+			case 'elron': {
+				return await elron.getRoute(id);
+			}
 			
 		}
 		
-		case 'elron': {
-			return void res.send(await elron.getRoute(id));
-		}
-		
-	}
+	}));
 	
 }
 
