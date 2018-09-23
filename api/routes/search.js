6fedfca7e040d5da502e59ba8d36355fdc430bd4
Karl Köörna <karlkoorna@gmail.com>
2018-09-23 14:10:14 +0300

    Optimize route search (api)

--- api/routes/search.js
+++ api/routes/search.js
@@ -10,7 +10,7 @@ async function getSearch(req, res) {
 		db.query(`
 			SELECT
 				id, name, direction, type
-				${lat && lng ? ", ST_DISTANCE_SPHERE(POINT(lng, lat), POINT(?, ?)) AS distance" : ''}
+				${lat && lng ? ", ROUND(ST_DISTANCE_SPHERE(POINT(lng, lat), POINT(?, ?))) AS distance" : ''}
 			FROM stops
 			WHERE
 				type IS NOT NULL
@@ -19,19 +19,23 @@ async function getSearch(req, res) {
 			LIMIT 15
 		`, params),
 		db.query(`
-			SELECT
-				route_id AS id, route.name, origin, destination, route.type
-				${lat && lng ? ", ST_DISTANCE_SPHERE(POINT(stop.lng, stop.lat), POINT(?, ?)) AS distance" : ''}
-			FROM stop_routes
-			JOIN stops AS stop ON stop.id = stop_id
-			JOIN routes AS route ON route.id = route_id
+			SELECT id, name, origin, destination, route_type AS type
+			${lat && lng ? ', distance' : ''}
+			FROM (
+				SELECT
+					route_id AS id, route.name, origin, destination, route.type AS route_type, stop.type AS stop_type
+					${lat && lng ? ", ROUND(ST_DISTANCE_SPHERE(POINT(stop.lng, stop.lat), POINT(?, ?))) AS distance" : ''}
+				FROM stop_routes
+				JOIN stops AS stop ON stop.id = stop_id
+				JOIN routes AS route ON route.id = route_id
+				GROUP BY route_id
+				LIMIT 15
+			) AS stop_route
 			WHERE
-				stop.type IS NOT NULL
-				AND route.type IS NOT NULL
-				AND route.name LIKE ?
-			${lat && lng ? 'GROUP BY route_id' : ''}
-			ORDER BY ${lat && lng ? 'distance,' : ''} LENGTH(route.name), route.name
-			LIMIT 15
+				stop_type IS NOT NULL
+				AND route_type IS NOT NULL
+				AND name LIKE ?
+				ORDER BY ${lat && lng ? 'distance,' : ''} LENGTH(name), name
 		`, params)
 	]);
 	
