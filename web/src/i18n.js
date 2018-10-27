5c60d208a16d91ea63b27f7987cf8cf49b635b25
Karl Köörna <karlkoorna@gmail.com>
2018-10-27 20:31:13 +0300

    (web) Include locales in bundle for faster loading

--- web/src/i18n.js
+++ web/src/i18n.js
@@ -3,17 +3,26 @@ import XHR from 'i18next-xhr-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { reactI18nextModule } from 'react-i18next';
 
-i18n.use(XHR)
-	.use(LanguageDetector)
+import en from './locales/en.json';
+import et from './locales/et.json';
+import ru from './locales/ru.json';
+
+function genResouces(langs) {
+	const resources = {};
+	for (const lang of Object.entries(langs)) resources[lang[0]] = { translation: lang[1] };
+	return resources;
+}
+
+i18n.use(LanguageDetector)
 	.use(reactI18nextModule)
 	.init({
+		resources: genResouces({
+			en, et, ru
+		}),
 		load: 'languageOnly',
 		fallbackLng: 'en',
 		interpolation: {
 			escapeValue: false
-		},
-		backend: {
-			loadPath: '/locales/{{lng}}.json'
 		},
 		detection: {
 			lookupQuerystring: 'lang',
