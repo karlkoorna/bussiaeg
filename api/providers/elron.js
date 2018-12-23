4445307a5a80b58f75a0b9c32bc479cd30e1a7d7
Karl Köörna <karlkoorna@gmail.com>
2018-12-23 12:49:12 +0200

    Add caching to Elron provider (api)

--- api/providers/elron.js
+++ api/providers/elron.js
@@ -7,24 +7,25 @@ const time = require('../utils/time.js');
 async function getTrips(id) {
 	
 	const now = time.getSeconds();
-	const data = JSON.parse((await got(`http://elron.ee/api/v1/stop?stop=${encodeURIComponent(id)}`)).body).data;
 	
-	if (!data) throw new Error("Provider 'Elron' is not returning data");
-	if (data.text) throw new Error(data.text);
-	
-	const trips = [];
-	
-	for (const trip of data) if (time.toSeconds(trip.plaaniline_aeg) > now) trips.push({
-		time: time.toSeconds(trip.plaaniline_aeg),
-		countdown: time.toSeconds(trip.plaaniline_aeg) - now,
-		name: trip.reis,
-		destination: trip.liin,
-		type: 'train',
-		live: false,
-		provider: 'elron'
-	});
-	
-	return trips.slice(0, 15);
+	return (await cache.use('elron-trips', id, async () => {
+		
+		const data = JSON.parse((await got(`http://elron.ee/api/v1/stop?stop=${encodeURIComponent(id)}`)).body).data;
+		
+		if (!data) throw new Error("Provider 'Elron' is not returning data");
+		if (data.text) throw new Error(data.text);
+		
+		return data.map((trip) => ({
+			time: time.toSeconds(trip.plaaniline_aeg),
+			countdown: time.toSeconds(trip.plaaniline_aeg) - now,
+			name: trip.reis,
+			destination: trip.liin,
+			type: 'train',
+			live: false,
+			provider: 'elron'
+		}));
+		
+	})).filter((trip) => trip.time > now).slice(0, 15);
 	
 }
 
