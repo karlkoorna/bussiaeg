904dfca5b456e5dfa29c4e6e69985b3a34e5a96e
Karl Köörna <karlkoorna@gmail.com>
2018-12-23 23:13:30 +0200

    Filled settings view (web)

--- web/src/views/Settings/Settings.jsx
+++ web/src/views/Settings/Settings.jsx
@@ -7,7 +7,25 @@ export default class Settings extends Component {
 	render() {
 		return (
 			<main id="settings" className="view">
-				Settings
+				<label><i className="material-icons">language</i>Language</label>
+				<select>
+					<option>Eesti keel</option>
+					<option>English</option>
+					<option>Русскии</option>
+				</select>
+				<label><i className="material-icons">style</i>Theme</label>
+				<select>
+					<option>Light</option>
+					<option>Dark</option>
+				</select>
+				<label><i className="material-icons">view_carousel</i>Default view</label>
+				<select>
+					<option>Search</option>
+					<option>Favorites</option>
+					<option>Map</option>
+				</select>
+				<label><i className="material-icons">search</i>Stop zoom</label>
+				<input type="number" min="0" max="18"></input>
 			</main>
 		);
 	}
