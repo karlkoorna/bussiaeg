ce16800987c676a1b5026007a46e5db889683da3
Karl Köörna <karlkoorna@gmail.com>
2018-12-23 12:49:40 +0200

    Add MNT semiprovider (api)

---
+++ api/providers/mnt.js
@@ -0,0 +1,36 @@
+const db = require('../db.js');
+
+const cache = require('../utils/cache.js');
+
+async function getTrips(id, coachOnly) {
+	
+	return await cache.use('mnt-trips', id, () => db.query(`
+		SELECT TIME_TO_SEC(time) AS time, TIME_TO_SEC(time) - TIME_TO_SEC(NOW()) as countdown, route.name, trip.destination, route.type, FALSE AS live, 'mnt' AS provider FROM stops AS stop
+			JOIN stop_times ON stop_id = id
+			JOIN trips AS trip ON trip.id = trip_id
+			JOIN routes AS route ON route.id = route_id
+			JOIN services AS service ON service.id = trip.service_id
+		WHERE
+			stop.id = ?
+			AND route.type IS NOT NULL
+			AND time > CURTIME()
+			AND (
+				(
+					CURDATE() BETWEEN start AND end
+					AND SUBSTR(days, WEEKDAY(CURDATE()) + 1, 1) = 1
+					AND service_id NOT IN (
+						SELECT service_id FROM service_exceptions WHERE type = 0 AND date = CURDATE()
+					)
+				) OR service_id IN (
+					SELECT service_id FROM service_exceptions WHERE type = 1 AND date = CURDATE()
+				)
+			)
+			${coachOnly ? "AND route.type LIKE 'coach%'" : ''}
+		LIMIT ${coachOnly ? '5' : '15'}
+	`, [ id ]));
+	
+}
+
+module.exports = {
+	getTrips
+};
--- api/routes/trips.js
+++ api/routes/trips.js
@@ -2,60 +2,31 @@ const got = require('got');
 
 const db = require('../db.js');
 
-const cache = require('../utils/cache.js');
 const time = require('../utils/time.js');
 
 const elron = require('../providers/elron.js');
 const tlt = require('../providers/tlt.js');
+const mnt = require('../providers/mnt.js');
 
 // Get trips for stop.
 async function getTrips(req, res) {
 	
-	const { stop_id: stopId } = req.query;
+	const { stop_id: id } = req.query;
 	const now = time.getSeconds();
 	let trips = [];
 	
 	// Verify stop, get type and region.
-	const stop = (await db.query('SELECT type, region FROM stops WHERE id = ?', [ stopId ]))[0];
-	if (!stop) throw new Error(`Stop with id '${stopId}' not found`);
+	const stop = (await db.query('SELECT type, region FROM stops WHERE id = ?', [ id ]))[0];
+	if (!stop) throw new Error(`Stop with id '${id}' not found`);
 	
 	// Elron
 	
-	if (stop.type === 'train') return res.send(await elron.getTrips(stopId));
+	if (stop.type === 'train') return res.send(await elron.getTrips(id));
 	
 	// TLT + MNT
 	
-	if (stop.region === 'tallinn') trips = await tlt.getTrips(stopId);
-	
-	const mntTrips = await cache.use('mnt-stops', stopId, () => db.query(`
-		SELECT TIME_TO_SEC(time) AS time, route.name, trip.destination, route.type, FALSE AS live, 'mnt' AS provider FROM stops AS stop
-			JOIN stop_times ON stop_id = id
-			JOIN trips AS trip ON trip.id = trip_id
-			JOIN routes AS route ON route.id = route_id
-			JOIN services AS service ON service.id = trip.service_id
-		WHERE
-			stop.id = ?
-			AND route.type IS NOT NULL
-			AND time > CURTIME()
-			AND (
-				(
-					CURDATE() BETWEEN start AND end
-					AND SUBSTR(days, WEEKDAY(CURDATE()) + 1, 1) = 1
-					AND service_id NOT IN (
-						SELECT service_id FROM service_exceptions WHERE type = 0 AND date = CURDATE()
-					)
-				) OR service_id IN (
-					SELECT service_id FROM service_exceptions WHERE type = 1 AND date = CURDATE()
-				)
-			)
-			${trips.length ? "AND route.type LIKE 'coach%'" : ''}
-		LIMIT ${trips.length ? '5' : '15'}
-	`, [ stopId ]));
-	
-	trips.concat(trips.concat(mntTrips).map((trip) => ({
-		...trip,
-		countdown: trip.time - time.getSeconds()
-	})).sort((a, b) => a.time - b.time));
+	if (stop.region === 'tallinn') trips = await tlt.getTrips(id);
+	trips = trips.concat(await mnt.getTrips(id, trips.length)).sort((a, b) => a.time - b.time);
 	
 	res.send(trips);
 	
