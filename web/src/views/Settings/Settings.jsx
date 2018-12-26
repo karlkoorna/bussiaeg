1ff32c751b6d2b9741ea3fec3c793f9be1bd5c95
Karl Köörna <karlkoorna@gmail.com>
2018-12-26 14:37:46 +0200

    Hide keyboard on enter and add select arrow in Settings (web)

--- web/src/views/Settings/Settings.jsx
+++ web/src/views/Settings/Settings.jsx
@@ -10,9 +10,13 @@ import './Settings.css';
 
 class Settings extends Component {
 	
+	hideKeyboard = (e) => {
+		if (e.which === 13) e.target.blur();
+	}
+	
 	updateSetting = (e) => {
 		const target = e.target;
-		this.props.storeSettings.update(target.name, target.valueAsNumber || target.value || target.options[e.target.selectedIndex].value, true);
+		this.props.storeSettings.update(target.name, target.valueAsNumber || target.value || target.options ? target.options[e.target.selectedIndex].value : this.props.storeSettings.defaultData[target.name], true);
 	}
 	
 	render() {
@@ -25,27 +29,27 @@ class Settings extends Component {
 					<meta name="theme-color" content={viewColors.settings[0]} />
 				</Helmet>
 				<main id="settings" className="view">
-					<label><i className="material-icons">language</i>{t('settings.language')}<span>({defaultData.lang})</span></label>
+					<label className="is-select"><i className="material-icons">language</i>{t('settings.language')}<span>({defaultData.lang})</span></label>
 					<select name="lang" defaultValue={data.lang} onInput={this.updateSetting}>
 						<option value="et">Eesti keel</option>
 						<option value="en">English</option>
 						<option value="ru">Русский</option>
 					</select>
-					<label><i className="material-icons">style</i>{t('settings.theme')}<span>({t(`settings.theme-${defaultData.theme}`)})</span></label>
+					<label className="is-select"><i className="material-icons">style</i>{t('settings.theme')}<span>({t(`settings.theme-${defaultData.theme}`)})</span></label>
 					<select name="theme" defaultValue={data.theme} onInput={this.updateSetting}>
 						<option value="light">{t('settings.theme-light')}</option>
 						<option value="dark">{t('settings.theme-dark')}</option>
 					</select>
-					<label><i className="material-icons">view_carousel</i>{t('settings.view')}<span>({t(`settings.view-${defaultData.view}`)})</span></label>
+					<label className="is-select"><i className="material-icons">view_carousel</i>{t('settings.view')}<span>({t(`settings.view-${defaultData.view}`)})</span></label>
 					<select name="view" defaultValue={data.view} onInput={this.updateSetting}>
 						<option value="search">{t('settings.view-search')}</option>
 						<option value="favorites">{t('settings.view-favorites')}</option>
 						<option value="map">{t('settings.view-map')}</option>
 					</select>
 					<label><i className="material-icons">search</i>{t('settings.startZoom')}<span>({defaultData.startZoom})</span></label>
-					<input name="startZoom" defaultValue={data.startZoom} type="number" min={mapOpts.minZoom} max={mapOpts.maxZoom} onInput={this.updateSetting}></input>
+					<input name="startZoom" defaultValue={data.startZoom} type="number" min={mapOpts.minZoom} max={mapOpts.maxZoom} onKeyDown={this.hideKeyboard} onInput={this.updateSetting}></input>
 					<label><i className="material-icons">visibility</i>{t('settings.stopZoom')}<span>({defaultData.stopZoom})</span></label>
-					<input name="stopZoom" defaultValue={data.stopZoom} type="number" min={mapOpts.minZoom} max={mapOpts.maxZoom} onInput={this.updateSetting}></input>
+					<input name="stopZoom" defaultValue={data.stopZoom} type="number" min={mapOpts.minZoom} max={mapOpts.maxZoom} onKeyDown={this.hideKeyboard} onInput={this.updateSetting}></input>
 					<img src="https://raw.githubusercontent.com/karlkoorna/bussiaeg/master/web/public/assets/banner-1.svg?sanitize=true" alt="Bussiaeg.ee" />
 					<p>
 						<a rel="nofollow" target="_blank" href="https://mnt.ee">Maanteeamet</a> ― Plaaniajad.<br />
