5d11726acdc597d3bb0800338ba78c16728820ea
Karl Köörna <karlkoorna@gmail.com>
2019-01-08 16:26:00 +0200

    Fix caching time dependant MNT provider query (api)

--- api/providers/mnt.js
+++ api/providers/mnt.js
@@ -1,10 +1,8 @@
 const db = require('../db.js');
 
-const cache = require('../utils/cache.js');
-
 async function getTrips(id, coachOnly) {
 	
-	return await cache.use('mnt-trips', id, async () => (await db.query(`
+	return (await db.query(`
 		SELECT TIME_TO_SEC(time) AS time, TIME_TO_SEC(time) - TIME_TO_SEC(NOW()) as countdown, route.name, trip.destination, route.type, 'mnt' AS provider FROM stops AS stop
 			JOIN stop_times ON stop_id = id
 			JOIN trips AS trip ON trip.id = trip_id
@@ -28,7 +26,7 @@ async function getTrips(id, coachOnly) {
 			${coachOnly ? "AND route.type LIKE 'coach%'" : ''}
 		ORDER BY time
 		LIMIT ${coachOnly ? '5' : '15'}
-	`, [ id ])).map((trip) => ({ ...trip, live: false })));
+	`, [ id ])).map((trip) => ({ ...trip, live: false }));
 	
 }
 
