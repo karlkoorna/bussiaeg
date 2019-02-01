b2a651bd70a7b8e3c64832f2f95aee8bdadf2dcf
Karl Köörna <karlkoorna@gmail.com>
2019-02-01 18:53:25 +0200

    Materialize Modal component (web)

--- web/src/components/Modal/Modal.jsx
+++ web/src/components/Modal/Modal.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { withNamespaces } from 'react-i18next';
 import { CSSTransition } from 'react-transition-group';
+import Ink from 'react-ink';
 
 import './Modal.css';
 
@@ -28,14 +29,20 @@ class Modal extends Component {
 		const { t, title, text, showCancel, isVisible, onCancel, onConfirm } = this.props;
 		
 		return (
-			<CSSTransition in={isVisible} classNames={{ enter: 'is-entering', exit: 'is-exiting' }} timeout={{ enter: 300, exit: 170 }} onExited={this.onExited} unmountOnExit>
-				<div id="modal-container" onMouseDown={onCancel}>
-					<div id="modal" onMouseDown={(e) => { e.stopPropagation() }}>
+			<CSSTransition in={isVisible} classNames={{ enter: 'is-entering', exit: 'is-exiting' }} timeout={{ enter: 150, exit: 75 }} onExited={this.onExited} unmountOnExit>
+				<div id="modal-container" onMouseUp={onCancel} onContextMenu={(e) => { e.preventDefault(); }}>
+					<div id="modal" onMouseUp={(e) => { e.stopPropagation(); }} onContextMenu={(e) => { e.stopPropagation(); }}>
 						<div id="modal-title">{title}</div>
 						<div id="modal-text">{text}</div>
 						<div id="modal-buttons">
-							{showCancel ? <div id="modal-buttons-cancel" onMouseDown={(e) => this.fire(e, onCancel)}>{t('modal.cancel')}</div> : null}
-							<div id="modal-buttons-confirm" onMouseDown={(e) => this.fire(e, onConfirm)}>{t('modal.confirm')}</div>
+							{showCancel ? (
+								<div id="modal-buttons-cancel" onMouseUp={(e) => this.fire(e, onCancel)}>{t('modal.cancel')}
+									<Ink background={false} opacity={.5} />
+								</div>
+							) : null}
+							<div id="modal-buttons-confirm" onMouseUp={(e) => this.fire(e, onConfirm)}>{t('modal.confirm')}
+								<Ink background={false} opacity={.5} />
+							</div>
 						</div>
 					</div>
 				</div>
