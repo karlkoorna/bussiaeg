0a5a42b53cc93f35fb45efbb3735393c03d468c5
Karl Köörna <karlkoorna@gmail.com>
2019-02-02 12:28:09 +0200

    Disable request retries in TLT provider (api)

--- api/providers/tlt.js
+++ api/providers/tlt.js
@@ -19,7 +19,7 @@ async function update(id) {
 	
 	try {
 		
-		const data = (await got(`https://transport.tallinn.ee/siri-stop-departures.php?stopid=${id || Object.keys(stops)}`)).body;
+		const data = (await got(`https://transport.tallinn.ee/siri-stop-departures.php?stopid=${id || Object.keys(stops)}`), { retry: 0 }).body;
 		
 		// Fallback to GTFS trips on error.
 		if (id || new Date() - last < 4200) if (data.split('\n').length === 2) {
