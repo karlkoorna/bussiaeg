9f11dc81dea4832e7552bf96f120b528e280a9f2
Karl Köörna <karlkoorna@gmail.com>
2019-05-26 19:23:58 +0300

    Add image lazy loading and CSS variables (docs)

--- docs/js/script.js
+++ docs/js/script.js
@@ -2,3 +2,16 @@
 addEventListener('load', () => {
 	document.body.classList.remove('is-loading');
 });
+
+// Lazy load images.
+const observer = new IntersectionObserver(((entries) => {
+	for (const entry of entries) if (entry.isIntersecting) {
+		const image = entry.target;
+		image.src = image.getAttribute('data-src');
+		image.classList.remove('is-lazy');
+		observer.unobserve(image);
+	}
+}));
+
+// Observe lazy images.
+for (const image of [ ...document.querySelectorAll('img.is-lazy') ]) observer.observe(image);
