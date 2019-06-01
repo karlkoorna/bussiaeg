21b32bc9973a827b7d0f0e922f25034f1ec0c951
Karl Köörna <karlkoorna@gmail.com>
2019-06-01 19:34:40 +0300

    Update views to work with new API routes (web)

--- web/src/views/Route/Route.jsx
+++ web/src/views/Route/Route.jsx
@@ -3,6 +3,7 @@ import { withRouter, Link } from 'react-router-dom';
 import { withTranslation } from 'react-i18next';
 import { Helmet } from 'react-helmet';
 
+import { formatTime, formatCountdown } from 'utils.js';
 import Loader from 'components/Loader/Loader.jsx';
 import Icon, { colors as iconColors } from 'components/Icon.jsx';
 
@@ -16,45 +17,56 @@ class ViewRoute extends Component {
 		route: {},
 		trips: {},
 		variant: 0,
+		stopId: null,
 		isLoading: true
 	};
 	
-	// Navigate to trip.
-	selectTrip = (e) => {
-		this.props.history.push(`/route?id=${this.state.route.id}&variant=${e.target.selectedIndex + 1}`);
+	// Focus stop by id.
+	focusStop = (id) => {
+		const el = document.querySelector(`.route-stops-stop[href$="${id}"]`);
+		if (!el) return;
+		
+		el.classList.add('is-active');
+		el.scrollIntoView({ behavior: 'smooth', block: 'center' });
 	}
 	
-	// Update stops on view navigation.
-	static getDerivedStateFromProps() {
-		return { variant: Math.max(Number((new URLSearchParams(window.location.search)).get('variant')) - 1, 0) || 0 };
+	// Navigate to trip.
+	selectTrip = (e) => {
+		this.setState({ variant: e.target.selectedIndex }, () => {
+			if (this.state.stopId) this.focusStop(this.state.stopId);
+		});
 	}
 	
 	// Fetch trips, redirect to default view if unsuccessful.
 	async componentDidMount() {
 		try {
 			const params = new URLSearchParams(window.location.search);
-			const route = await (await fetch(`${process.env['REACT_APP_API']}/routes?id=${params.get('id')}`)).json();
-			const trips = await (await fetch(`${process.env['REACT_APP_API']}/trips?route_id=${route.id}`)).json();
+			const route = await (await fetch(`${process.env['REACT_APP_API']}/routes/${params.get('id')}`)).json();
+			const trips = await (await fetch(`${process.env['REACT_APP_API']}/routes/${params.get('id')}/trips?id=${params.get('trip_id')}`)).json();
 			
 			this.setState({
 				route,
 				trips,
-				variant: Math.max(Number(params.get('variant')) - 1, 0) || 0,
+				variant: params.get('trip_id') ? Object.values(trips).reverse().findIndex((trip) => trip[0].time) : params.get('variant') || 0,
+				stopId: params.get('stop_id'),
 				isLoading: false
+			}, () => {
+				if (this.state.stopId) this.focusStop(this.state.stopId);
 			});
-		} catch (ex) {
+		} catch {
 			return void this.props.history.push('/' + storeSettings.data.view);
 		}
 	}
 	
 	render() {
 		const { route, trips, variant, isLoading } = this.state;
-		const descriptions = Object.keys(trips);
-		const description = descriptions[Math.min(variant, descriptions.length - 1)];
+		
+		const descriptions = Object.keys(trips).reverse();
+		const description = descriptions[Math.min(Math.max(variant, 0), descriptions.length - 1)];
 		
 		return (
 			<>
-				{route.id ? (
+				{route.name ? (
 					<Helmet>
 						<title>{route.name + ' – ' + description}</title>
 						<meta property="og:title" content={route.name + ' – ' + description} />
@@ -63,11 +75,11 @@ class ViewRoute extends Component {
 				) : null}
 				<main id="route" className="view">
 					<div id="route-info-bar" />
-					{route.id ? (
+					{route.name ? (
 						<div id="route-info">
 							<Icon id="route-info-icon" shape="vehicle" type={route.type} />
 							<div>
-								<div id="route-info-name">{route.name || ''}</div>
+								<div id="route-info-name">{route.name}</div>
 								<select id="route-info-description" defaultValue={description} onChange={this.selectTrip}>{descriptions.map((key) => <option key={key}>{key}</option>)}</select>
 							</div>
 						</div>
@@ -79,8 +91,14 @@ class ViewRoute extends Component {
 									<Icon className="route-stops-stop-icon" shape="stop" type={stop.type} />
 									<div>
 										<div className="route-stops-stop-name" style={{ color: iconColors[stop.type][0] }}>{stop.name}</div>
-										<div className="route-stops-stop-description" style={{ color: iconColors[stop.type][1] }}>{stop.description || (stop.origin && stop.destination ? `${stop.origin} - ${stop.destination}` : '')}</div>
+										<div className="route-stops-stop-description" style={{ color: iconColors[stop.type][1] }}>{stop.description}</div>
 									</div>
+									{stop.time ? (
+										<div>
+											<div className={'route-stops-stop-countdown' + (stop.countdown > 0 && stop.countdown < 60 ? ' is-urgent' : '') + (stop.countdown <= 0 ? ' has-arrived' : '')}>{formatCountdown(stop.countdown)}</div>
+											<div className="route-stops-stop-time">{formatTime(stop.time)}</div>
+										</div>
+									) : null}
 								</Link>
 							</li>
 						))}
