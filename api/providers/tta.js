55b22bdfcd1619134a7bab363a28a8a1b1fcaeca
Karl Köörna <karlkoorna@gmail.com>
2019-07-07 17:06:18 +0300

    Fix async TTA provider (api)

--- api/providers/tta.js
+++ api/providers/tta.js
@@ -4,47 +4,50 @@ const time = require('../utils/time.js');
 const cache = require('../utils/cache.js');
 const debug = require('../utils/debug.js');
 
+async function updateCache(id) {
+	try {
+		return (await got('https://transport.tallinn.ee/siri-stop-departures.php?stopid=' + id, { timeout: 1000, retry: 1 })).body.split('\n').map((line) => line.split(',')).slice(2, -1).map((departure) => ({
+			name: departure[1],
+			type: departure[0],
+			time: Number(departure[3]),
+			countdown: Number(departure[2] - 5)
+		}));
+	} catch (ex) {
+		debug.warn('Failed to fetch TTA data.', ex);
+		return [];
+	}
+}
+
 // Get trips for stop.
 async function getStopDepartures(id, mntDepartures) {
-	const ttaDepartures = await cache.use('tta', id) || [];
-	let departures = [];
+	const ttaDepartures = await cache.use('tta', id, updateCache.bind(this, id));
+	const departures = [];
 	
-	cache.use('tta', id, async () => {
-		try {
-			return (await got('https://transport.tallinn.ee/siri-stop-departures.php?stopid=' + id, { timeout: 1000, retry: 1 })).body.split('\n').map((line) => line.split(',')).slice(2).map((departure) => ({
-				name: departure[1],
-				type: departure[0],
-				time: Number(departure[3]),
-				countdown: Number(departure[2] - 5)
-			}));
-		} catch (ex) {
-			debug.warn('Failed to fetch TTA data.', ex);
-		}
-	});
+	// Asynchronously prefetch future TTA departures.
+	cache.use('tta', id, updateCache.bind(this, id));
 	
-	// Merge MNT trips with TTA trips if available.
-	if (ttaDepartures.length) for (const mntDeparture of mntDepartures) {
-		// Add coach trip.
-		if (mntDeparture.type.startsWith('coach') && mntDeparture.countdown > 0) {
+	// Merge MNT departures with TTA departures if available.
+	for (const mntDeparture of mntDepartures) {
+		// Add coach departure.
+		if (mntDeparture.type.startsWith('coach')) {
 			departures.push(mntDeparture);
 			continue;
 		}
 		
-		// Add matched trip.
-		const ttaTime = ttaDepartures.find((departure) => departure.name === mntDeparture.name && departure.type === mntDeparture.type && Math.abs(departure.time - mntDeparture.time) < 60);
-		if (!ttaTime) continue;
+		// Try to match departures.
+		const ttaDeparture = ttaDepartures.find((departure) => departure.name === mntDeparture.name && departure.type === mntDeparture.type && Math.abs(departure.time - mntDeparture.time) < 60);
 		
-		departures.push({
+		// Add (matched) departure.
+		if (ttaDeparture) departures.push({
 			...mntDeparture,
-			time: ttaTime.time,
-			countdown: ttaTime.countdown - time.getSeconds(),
-			live: ttaTime.time !== ttaTime.countdown,
+			time: ttaDeparture.time,
+			countdown: ttaDeparture.countdown - time.getSeconds(),
+			live: ttaDeparture.time !== ttaDeparture.countdown,
 			provider: 'tta'
-		});
-	} else departures = mntDepartures.filter((trip) => trip.countdown > 0);
+		}); else departures.push(mntDeparture);
+	}
 	
-	// Sort merged trips by countdown.
-	return departures.sort((prevDeparture, nextDeparture) => prevDeparture.countdown - nextDeparture.countdown);
+	return departures.filter((departure) => departure.countdown > 0).sort((prevDeparture, nextDeparture) => prevDeparture.countdown - nextDeparture.countdown);
 }
 
 module.exports = {
