104a5a3e35ff20b5951a54016e0db579ee2c5626
Karl Köörna <karlkoorna@gmail.com>
2019-07-20 16:13:23 +0300

    Checkout latest version only in production mode

--- update.sh
+++ update.sh
@@ -7,7 +7,9 @@ killall node
 # Discard local changes and pull latest release from GitHub.
 git reset --hard origin/master
 git pull
+if [ ! $1 ]; then
 git checkout tags/$(curl -s https://api.github.com/repos/karlkoorna/bussiaeg/releases/latest | grep -oP '(?<="tag_name": ")([0-9.]*)')
+fi
 
 # Make scripts executable.
 chmod +x update.sh
