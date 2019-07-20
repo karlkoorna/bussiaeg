4c8097f3484b347cdbff0e38414dac32236c4e61
Karl Köörna <karlkoorna@gmail.com>
2019-07-20 21:35:43 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Disable shell path expansion.
+export MSYS_NO_PATHCONV=1
+
 # Kill old processes.
 killall caddy
 killall node
@@ -13,9 +16,9 @@ if [ $1 ]; then # in development mode.
 		openssl genrsa -out ca.key
 		openssl genrsa -out tls.key
 		
-		MSYS_NO_PATHCONV=1 openssl req -new -x509 -days 3650 -key ca.key -out ca.crt -subj "/CN=Bussiaeg.ee"
-		MSYS_NO_PATHCONV=1 openssl req -new -key tls.key -out tls.csr -subj "/CN=*.devaeg.ee"
-		MSYS_NO_PATHCONV=1 openssl x509 -req -days 3650 -CA ca.crt -CAkey ca.key -CAcreateserial -in tls.csr -out tls.crt -extfile tls.cfg
+		openssl req -new -x509 -days 3650 -key ca.key -out ca.crt -subj "/CN=Bussiaeg.ee"
+		openssl req -new -key tls.key -out tls.csr -subj "/CN=*.devaeg.ee"
+		openssl x509 -req -days 3650 -CA ca.crt -CAkey ca.key -CAcreateserial -in tls.csr -out tls.crt -extfile tls.cfg
 		
 		rm ca.key
 		rm ca.srl
