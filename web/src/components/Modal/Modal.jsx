3570ca6cbd2e0b0ff674d85f8c3774bd5388be0e
Karl Köörna <karlkoorna@gmail.com>
2019-08-10 18:58:43 +0300

    Update modal styles (web)

--- web/src/components/Modal/Modal.jsx
+++ web/src/components/Modal/Modal.jsx
@@ -11,24 +11,40 @@ function stopPropagation(e) {
 
 class Modal extends Component {
 	
+	state = {
+		active: 0
+	}
+	
 	onCancel = (e) => {
+		this.setState({ active: 1 });
 		if (e.button === 0) this.props.onCancel();
 	}
 	
+	onConfirm = () => {
+		this.setState({ active: 2 });
+		this.props.onConfirm();
+	}
+	
+	// Reset active button once modal fades out.
+	onExited = () => {
+		this.setState({ active: 0 });
+	}
+	
 	render() {
-		const { t, title, text, isVisible, onCancel, onConfirm } = this.props;
+		const { t, title, text, isVisible } = this.props;
+		const { active } = this.state;
 		
 		return (
-			<CSSTransition in={isVisible} timeout={{ enter: 150, exit: 150 }} onExited={this.onExited} unmountOnExit>
+			<CSSTransition in={isVisible} timeout={{ enter: 150, exit: 375 }} onExited={this.onExited} unmountOnExit>
 				<div id="modal-container" onMouseDown={this.onCancel}>
 					<div id="modal" onMouseDown={stopPropagation}>
 						<div id="modal-title">{title}</div>
 						<div id="modal-text">{text}</div>
 						<div id="modal-buttons">
-							<div id="modal-buttons-cancel" onClick={onCancel}>{t('modal.cancel')}
+							<div id="modal-buttons-cancel" className={active === 1 ? 'is-active' : ''} onClick={this.onCancel}>{t('modal.cancel')}
 								<Ink background={false} opacity={.5} />
 							</div>
-							<div id="modal-buttons-confirm" onClick={onConfirm}>{t('modal.confirm')}
+							<div id="modal-buttons-confirm" className={active === 2 ? 'is-active' : ''} onClick={this.onConfirm}>{t('modal.confirm')}
 								<Ink background={false} opacity={.5} />
 							</div>
 						</div>
