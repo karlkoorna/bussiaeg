949adf83ef886018cf08d1d43c729a7ad6d73dc0
Karl Köörna <karlkoorna@gmail.com>
2019-11-10 12:19:43 +0200

    Update SQIPs

--- docs/sqip.sh
+++ docs/sqip.sh
@@ -2,7 +2,7 @@
 
 # Fetch Node and Go dependencies when needed.
 if [[ $1 ]]; then
-	npm install -g sqip@canary sqip-cli@canary sqip-plugin-pixels@canary
+	npm install -g sqip@canary sqip-cli@canary sqip-plugin-primitive@canary sqip-plugin-blur@canary
 	go get -u github.com/fogleman/primitive
 fi
 
@@ -15,7 +15,7 @@ for file in $(find img/**/*.jpg -printf "%p %k\n"); do
 	path=$(echo $file | cut -d . -f 1 | cut -c5-)
 	shapes=$(($(echo $file | cut -d " " -f 2) / 5))
 	
-	sqip -i img/$path.jpg -o img/$path.tmp -p pixels > /dev/null
+	sqip -i img/$path.jpg -o img/$path.tmp -p primitive -n $shapes -p blur -b 64 > /dev/null
 	echo -n "data:image/svg+xml;base64," > _includes/sqip/$path.sqip
 	base64 -w 0 img/$path.tmp >> _includes/sqip/$path.sqip
 	rm img/$path.tmp
